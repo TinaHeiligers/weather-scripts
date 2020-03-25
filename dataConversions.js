6132d12863da7ccaac42ecedf4d9288b55692b61
TinaHeiligers <tina.heiligers@gmail.com>
2020-03-24 17:45:30 -0700

    Converts data items using arrays

--- dataConversions.js
+++ dataConversions.js
@@ -1,45 +1,41 @@
 const fs = require('file-system')
-const dataFileRead = JSON.parse(fs.readFileSync("./data/data_2019.json"));
-const dateFileRead = JSON.parse(fs.readFileSync("./data/dates_2019.json"));
+const dataFileRead = JSON.parse(fs.readFileSync("./data/data_2019.json")); // an array
+const dateFileRead = JSON.parse(fs.readFileSync("./data/dates_2019.json")); // as array
 
-dataFileRead.forEach((element, index) => {
-  element.date = dateFileRead[index];
-  element.currently.time = String(element.currently.time * 1000);
-  element.daily.data[0].apparentTemperatureHighTime = String(element.daily.data[0].apparentTemperatureHighTime * 1000);
-  element.daily.data[0].apparentTemperatureLowTime = String(element.daily.data[0].apparentTemperatureLowTime * 1000);
-  element.daily.data[0].apparentTemperatureMaxTime = String(element.daily.data[0].apparentTemperatureMaxTime * 1000);
-  element.daily.data[0].apparentTemperatureMinTime = String(element.daily.data[0].apparentTemperatureMinTime * 1000);
-  element.daily.data[0].precipIntensityMaxTime = String(element.daily.data[0].precipIntensityMaxTime * 1000);
-  element.daily.data[0].sunriseTime = String(element.daily.data[0].sunriseTime * 1000);
-  element.daily.data[0].sunsetTime = String(element.daily.data[0].sunsetTime * 1000);
-  element.daily.data[0].temperatureHighTime = String(element.daily.data[0].temperatureHighTime * 1000);
-  element.daily.data[0].temperatureLowTime = String(element.daily.data[0].temperatureLowTime * 1000);
-  element.daily.data[0].temperatureMaxTime = String(element.daily.data[0].temperatureMaxTime * 1000);
-  element.daily.data[0].temperatureMinTime = String(element.daily.data[0].temperatureMinTime * 1000);
-  element.daily.data[0].time = String(element.daily.data[0].time * 1000);
-  element.daily.data[0].uvIndexTime = String(element.daily.data[0].uvIndexTime * 1000);
-  element.daily.data[0].windGustTime = String(element.daily.data[0].windGustTime * 1000);
-  element.hourly.data.forEach(item => item.time = String(item.time * 1000));
-  if (index === 0) {
-    console.log('element', element)
-    console.log("element.currently.time", element.currently.time)
-    console.log("element.currently.time", element.currently.time)
-    console.log("element.daily.data[0].apparentTemperatureHighTime", element.daily.data[0].apparentTemperatureHighTime)
-    console.log("element.daily.data[0].apparentTemperatureLowTime", element.daily.data[0].apparentTemperatureLowTime)
-    console.log("element.daily.data[0].apparentTemperatureMaxTime", element.daily.data[0].apparentTemperatureMaxTime)
-    console.log("element.daily.data[0].apparentTemperatureMinTime", element.daily.data[0].apparentTemperatureMinTime)
-    console.log("element.daily.data[0].precipIntensityMaxTime", element.daily.data[0].precipIntensityMaxTime)
-    console.log("element.daily.data[0].sunriseTime", element.daily.data[0].sunriseTime)
-    console.log("element.daily.data[0].sunsetTime", element.daily.data[0].sunsetTime)
-    console.log("element.daily.data[0].temperatureHighTime", element.daily.data[0].temperatureHighTime)
-    console.log("element.daily.data[0].temperatureLowTime", element.daily.data[0].temperatureLowTime)
-    console.log("element.daily.data[0].temperatureMaxTime", element.daily.data[0].temperatureMaxTime)
-    console.log("element.daily.data[0].temperatureMinTime", element.daily.data[0].temperatureMinTime)
-    console.log("element.daily.data[0].time", element.daily.data[0].time)
-    console.log("element.daily.data[0].uvIndexTime", element.daily.data[0].uvIndexTime)
-    console.log("element.daily.data[0].windGustTime", element.daily.data[0].windGustTime)
-    element.hourly.data.forEach(item => item.time = console.log(item.time))
+const arrayOfConvertedData = dataFileRead.map((element, index) => {
+  // the element is an object
+  const convertedDataItem = {
+    ...element,
+    date: dateFileRead[index],
+    currently: {
+      ...element.currently,
+      time: element.currently.time * 1000,
+    },
+    daily: {
+      ...element.daily,
+      data: element.daily.data[0],
+      data: {
+        ...element.daily.data[0],
+        apparentTemperatureHighTime: element.daily.data[0].apparentTemperatureHighTime * 1000,
+        apparentTemperatureLowTime: element.daily.data[0].apparentTemperatureLowTime * 1000,
+        apparentTemperatureMaxTime: element.daily.data[0].apparentTemperatureMaxTime * 1000,
+        apparentTemperatureMinTime: element.daily.data[0].apparentTemperatureMinTime * 1000,
+        precipIntensityMaxTime: element.daily.data[0].precipIntensityMaxTime * 1000,
+        sunriseTime: element.daily.data[0].sunriseTime * 1000,
+        sunsetTime: element.daily.data[0].sunsetTime * 1000,
+        temperatureHighTime: element.daily.data[0].temperatureHighTime * 1000,
+        temperatureLowTime: element.daily.data[0].temperatureLowTime * 1000,
+        temperatureMaxTime: element.daily.data[0].temperatureMaxTime * 1000,
+        temperatureMinTime: element.daily.data[0].temperatureMinTime * 1000,
+        time: element.daily.data[0].time * 1000,
+        uvIndexTime: element.daily.data[0].uvIndexTime * 1000,
+        windGustTime: element.daily.data[0].windGustTime * 1000,
+      }
+    },
+    hourly: {
+      data: element.hourly.data.map(item => item.time = item.time * 1000)
+    }
   }
-  // fs.writeSync(openedDataFile, JSON.stringify(element) + '\n')
+  return convertedDataItem;
 });
-// fs.closeSync(openedDataFile);
+console.log(arrayOfConvertedData[0])
--- toJsonl.js
+++ toJsonl.js
@@ -7,25 +7,7 @@ const openedDataFile = fs.openSync("./data/data_2020.jsonl", "w");
 dataFileRead.forEach((element, index) => {
   // TODO: make transformations to the element's elements here.
 
-  element.date = dateFileRead[index];
 
-  element.date = dateFileRead[index];
-  element.currently.time = String(element.currently.time * 1000);
-  element.daily.data[0].apparentTemperatureHighTime = String(element.daily.data[0].apparentTemperatureHighTime * 1000);
-  element.daily.data[0].apparentTemperatureLowTime = String(element.daily.data[0].apparentTemperatureLowTime * 1000);
-  element.daily.data[0].apparentTemperatureMaxTime = String(element.daily.data[0].apparentTemperatureMaxTime * 1000);
-  element.daily.data[0].apparentTemperatureMinTime = String(element.daily.data[0].apparentTemperatureMinTime * 1000);
-  element.daily.data[0].precipIntensityMaxTime = String(element.daily.data[0].precipIntensityMaxTime * 1000);
-  element.daily.data[0].sunriseTime = String(element.daily.data[0].sunriseTime * 1000);
-  element.daily.data[0].sunsetTime = String(element.daily.data[0].sunsetTime * 1000);
-  element.daily.data[0].temperatureHighTime = String(element.daily.data[0].temperatureHighTime * 1000);
-  element.daily.data[0].temperatureLowTime = String(element.daily.data[0].temperatureLowTime * 1000);
-  element.daily.data[0].temperatureMaxTime = String(element.daily.data[0].temperatureMaxTime * 1000);
-  element.daily.data[0].temperatureMinTime = String(element.daily.data[0].temperatureMinTime * 1000);
-  element.daily.data[0].time = String(element.daily.data[0].time * 1000);
-  element.daily.data[0].uvIndexTime = String(element.daily.data[0].uvIndexTime * 1000);
-  element.daily.data[0].windGustTime = String(element.daily.data[0].windGustTime * 1000);
-  element.hourly.data.forEach(item => item.time = String(item.time * 1000));
   fs.writeSync(openedDataFile, JSON.stringify(element) + '\n')
 });
 fs.closeSync(openedDataFile);
